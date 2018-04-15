4171b9e782d6b925c5f7d5ded52fa061b35c1b0e
Irfaan <irfaankhodabukus@gmail.com>
2018-04-15 22:34:13 +0200

    correction of code

--- components/map/Geolocalisation.js
+++ components/map/Geolocalisation.js
@@ -1,98 +1,70 @@
-import React, { Component } from 'react'
-import { Platform, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Constants, Location, Permissions } from 'expo'
-import MapView, { Marker } from 'react-native-maps'
-
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet, TouchableOpacity, TouchableHighlight, Button } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
+import MapView, { Marker } from 'react-native-maps';
+import Info from './Info';
 const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
 
 export default class Geolocalisation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markers: [{
-        title: 'hello',
+      //SAMPLE MARKERS
+      markers: [
+        {
+        title: 'hello ECV',
         coordinates: {
           latitude: 48.8852732,
           longitude: 2.3755954
+          },
         },
-      },
-      {
+        {
         title: 'hello',
         coordinates: {
           latitude: 48.8857688,
           longitude: 2.3759105
-        },  
-      }],
+          },  
+        }
+    ],
         location: { coords: {latitude: 0, longitude: 0}},
       };
   }
-   
-    
-      componentWillMount() {
-        Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+     
+  componentWillMount() {
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+  }
+  
+  locationChanged = (location) => {
+    region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.05,
       }
-    
-      locationChanged = (location) => {
-        region = 
-        {
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.05,
-        }
+    this.setState({
+      location, 
+      region
+    })
+  }
 
-          this.setState({
-            location, 
-            region
-          })
-
-      }
+  markerClick() {
+      this.props.navigation.navigate('Info')
+  }
       
   render() {
     return (
-        <MapView
-          style={{ flex: 1}}
-          showsUserLocation={true}
-          region={this.state.region}
-        >
-      {this.state.markers.map(marker => (
-        <MapView.Marker 
+      <MapView
+        style={{ flex: 1}}
+        showsUserLocation={true}
+        region={this.state.region}>
+        
+      {this.state.markers.map( (marker) => (
+        <MapView.Marker
           coordinate={marker.coordinates}
-          title={marker.title}
-        />
+          title={marker.title} onPress={() => this.markerClick()}>
+        </MapView.Marker>
       ))}
-        </MapView>
+      </MapView>
     )
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  bubble: {
-    backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
-  },
-});
